17654e96b295e212d423595ba399662cb97f14f7
antaechan <0627antaechan@gmail.com>
2022-07-07 21:40:30 +0900

    Implement basic Login page UI

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>q111111dfgsgOpen up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textInput} placeholder="Enter your ID" />
+      <TextInput style={styles.textInput} placeholder="Enter your Password" />
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          style={styles.clickButton}
+          onPress={() => Alert.alert("you click login button!")}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.clickButton}
+          onPress={() => Alert.alert("you click Assign button!")}
+        >
+          <Text style={styles.buttonText}>Assign</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +35,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "white",
+    height: 50,
+    width: 200,
+  },
+  buttonView: {
+    flexDirection: "row",
+  },
+  clickButton: {
+    width: 80,
+    height: 30,
+    margin: 10,
+    paddingTop: 5,
+    backgroundColor: "blue",
+    fontSize: 30,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
   },
 });
